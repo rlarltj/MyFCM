86f1a0981cfbf697bf8102a97ffed6fc5ac740da
김기서 <5rne@naver.com>
2023-04-25 23:56:09 +0900

    feat: EmitterRepository 생성

---
+++ src/main/java/com/example/myfcm/mysse/repository/EmitterRepository.java
@@ -0,0 +1,59 @@
+package com.example.myfcm.mysse.repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Repository
+public class EmitterRepository {
+	public final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+	private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
+
+	public SseEmitter save(String id, SseEmitter sseEmitter) {
+		emitters.put(id, sseEmitter);
+		return sseEmitter;
+	}
+
+	public void saveEventCache(String eventCacheId, Object event) {
+		eventCache.put(eventCacheId, event);
+	}
+
+	public Map<String, SseEmitter> findAllStartWithById(String userId) {
+		return emitters.entrySet().stream()
+			.filter(entry -> entry.getKey().startsWith(userId))
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
+
+	public Map<String, Object> findAllEventCacheStartWithId(String userId) {
+		return eventCache.entrySet().stream()
+			.filter(entry -> entry.getKey().startsWith(userId))
+			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+	}
+
+	public void deleteAllStartWithId(String userId) {
+		emitters.forEach(
+			(key, emitter) -> {
+				if (key.startsWith(userId)) {
+					emitters.remove(key);
+				}
+			}
+		);
+	}
+
+	public void deleteById(String id) {
+		emitters.remove(id);
+	}
+
+	public void deleteAllEventCacheStartWithId(String userId) {
+		eventCache.forEach(
+			(key, data) -> {
+				if (key.startsWith(userId)) {
+					eventCache.remove(key);
+				}
+			}
+		);
+	}
+}
